0271debdd47a303935d8dfaa6e5260d7abee6b51
karishmarajput <84656423+karishmarajput@users.noreply.github.com>
2022-01-18 20:41:48 +0530

    Update game.js

--- game.js
+++ game.js
@@ -22,7 +22,7 @@ let gameOver = false;
 function main(currentTime){
     if(gameOver){
         if(confirm('You lost. Press ok to restart.')){
-            window.location='/snake-game/'
+           location.reload();
         }
         return;
     }
